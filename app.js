53b25b6b2fd1f439a1030d7eb44936033a3502fb
Utsav Apurva <76875307+speeeedy-899@users.noreply.github.com>
2021-06-22 12:47:12 +0530

    Add app,js

---
+++ app.js
@@ -0,0 +1,12 @@
+
+// Preloader functions and actions
+
+window.addEventListener("load", function(){
+  const preload = document.querySelector(".preload");
+  preload.classList.add("preload-finish");
+  // preload.slideUp(default, 400);
+});
+
+
+
+// Preloader functions end
